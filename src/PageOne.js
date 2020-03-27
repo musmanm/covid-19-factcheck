b50439b5a751aa9c5d1203e75efe358efc88fd7b
Muhammad Usman Majeed <12mm123@queensu.ca>
2020-03-27 19:49:28 -0400

    progress 1

---
+++ src/PageOne.js
@@ -0,0 +1,19 @@
+import React, { Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './NavBar.js'
+import SideMenu from './SideMenu.js'
+
+
+class PageOne extends Component {
+  state = {sampleState: 'hello world'};
+
+  render(){
+     const { state } = this;
+     const setState = state => this.setState(state);
+
+     return <div onClick={() => setState({sampleState: 1})}>{state.sampleState}</div>;
+  }
+}
+
+export default PageOne;
